0a3e44d6f191c0070222506e1ded622d37741c81
Marcelo Amador <marcelojaf@gmail.com>
2019-11-15 15:23:39 -0500

    Initial commit

---
+++ src/CoordinatesLib.cs
@@ -0,0 +1,34 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MovingCoordinates
+{
+    /// <summary>
+    /// Based on https://stackoverflow.com/questions/36598981/calculating-a-coordinate-in-an-angle-and-distance-from-a-coordinate?noredirect=1&lq=1
+    /// </summary>
+    public class CoordinatesLib
+    {
+        public GeoCoordinate Offset(GeoCoordinate coordinate, double angle, double distanceInMeters)
+        {
+            double R = 6378100; //Radius of the Earth in meters
+            double rad = Math.PI * angle / 180;
+
+            double latitude = coordinate.Latitude * Math.PI / 180;
+            double longitude = coordinate.Longitude * Math.PI / 180;
+
+
+            double x = Math.Asin(Math.Sin(latitude) * Math.Cos(distanceInMeters / R)
+                                          + Math.Cos(latitude) * Math.Sin(distanceInMeters / R) * Math.Cos(rad));
+
+            double y = longitude + Math.Atan2(Math.Sin(rad) * Math.Sin(distanceInMeters / R) * Math.Cos(latitude), Math.Cos(distanceInMeters / R) - Math.Sin(latitude) * Math.Sin(x));
+
+            x = x * 180 / Math.PI; // convert back to degrees
+            y = y * 180 / Math.PI;
+
+            return new GeoCoordinate(x, y);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,62 @@
+﻿using Nest;
+using System;
+using System.Drawing;
+
+namespace MovingCoordinates
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double centerLatitude = 0.000000;
+            double centerLongitude = 0.000000;
+            int distanceInMeters = 0;
+
+            var lib = new CoordinatesLib();
+
+            Console.WriteLine("GET SQUARE BOUNDING BOX COORDINATES FROM A LOCATION AND RADIUS(IN METERS)!");
+            Console.WriteLine("DECIMAL PLACES = 6");
+            Console.WriteLine("Enter the location latitude:");
+            try
+            {
+                centerLatitude = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception error)
+            { 
+                throw new Exception(error.Message);
+            }
+            Console.WriteLine("Enter the location longitude:");
+            try
+            {
+                centerLongitude = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+            Console.WriteLine("Enter the radius distance in meters:");
+            try
+            {
+                distanceInMeters = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+
+            GeoCoordinate topCenter = lib.Offset(new GeoCoordinate(centerLatitude, centerLongitude), 0, distanceInMeters);
+            GeoCoordinate bottomCenter = lib.Offset(new GeoCoordinate(centerLatitude, centerLongitude), 180, distanceInMeters);
+
+            GeoCoordinate topLeftCoordinates = lib.Offset(new GeoCoordinate(topCenter.Latitude, topCenter.Longitude), 270, distanceInMeters);
+            GeoCoordinate topRightCoordinates = lib.Offset(new GeoCoordinate(topCenter.Latitude, topCenter.Longitude), 90, distanceInMeters);
+            GeoCoordinate bottomLeftCoordinates = lib.Offset(new GeoCoordinate(bottomCenter.Latitude, bottomCenter.Longitude), 270, distanceInMeters);
+            GeoCoordinate bottomRightCoordinates = lib.Offset(new GeoCoordinate(bottomCenter.Latitude, bottomCenter.Longitude), 90, distanceInMeters);
+
+            Console.WriteLine("The 4 coordinates for the bounding box with the location provided are:");
+            Console.WriteLine($"Top-Left: Latitude:{String.Format("{0:0.000000}",topLeftCoordinates.Latitude)} | Longitude: {String.Format("{0:0.000000}", topLeftCoordinates.Longitude)}");
+            Console.WriteLine($"Top-Right: Latitude:{String.Format("{0:0.000000}", topRightCoordinates.Latitude)} | Longitude: {String.Format("{0:0.000000}", topRightCoordinates.Longitude)}");
+            Console.WriteLine($"Bottom-Left: Latitude:{String.Format("{0:0.000000}", bottomLeftCoordinates.Latitude)} | Longitude: {String.Format("{0:0.000000}", bottomLeftCoordinates.Longitude)}");
+            Console.WriteLine($"Bottom-Right: Latitude:{String.Format("{0:0.000000}", bottomRightCoordinates.Latitude)} | Longitude: {String.Format("{0:0.000000}", bottomRightCoordinates.Longitude)}");
+        }
+    }
+}
